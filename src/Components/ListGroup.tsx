f34e7bc7bf2e51633abea1ea93ec39f2f87ecc57
Bibechana8 <bibechanapoudel25@gmail.com>
2026-06-20 18:59:45 +0545

    somechanges

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,9 +1,20 @@
-function ListGroup(){
-    const items = ["New York","San Francisco","Tokyo","London"];
-    return(
-        <ul className="list-group">
-            {items.map(item => <li className="list-group-item">{item}</li>)}
-        </ul>
-    );
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London"];
+  return (
+    <>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
+      </ul>
+      <ul className="list-group mt-3">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
